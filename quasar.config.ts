c331017790c43d6f3998a863ebe9c86f52cddfa4
joaogabriels <joaogabrielsevangelista@gmail.com>
2025-02-13 20:59:24 -0300

    refactor(quasar.config): altera config do quasar

--- quasar.config.ts
+++ quasar.config.ts
@@ -62,6 +62,8 @@ export default defineConfig((/* ctx */) => {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
+      publicPath: '/hotel/',
+
       vitePlugins: [
         [
           'vite-plugin-checker',
