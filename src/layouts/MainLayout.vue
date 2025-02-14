bbfeca21301c3dff22079ceb5e887837f83d6956
joaogabriels <joaogabrielsevangelista@gmail.com>
2025-02-13 21:39:43 -0300

    refactor(MainLayout): altera logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,8 @@
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>
-          Onfly
+          <q-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvb1aW5SbNJAGXTGNYpeKUeiLG-d0vmXjhlw&s"
+            alt="Onfly" />
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
